09b7577fddba3b9332eb3810bd7c065e2e0c29e0
MrGrayTheET <sandersnicholas68@gmail.com>
2025-09-03 12:09:52 -0500

    Updated COTs with Financials

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from forecaster.forecast import CTAForecast
 
 forecast = CTAForecast("CL_F")
 forecast.prepare_features(resample_after=True,
-                          resample_day="Friday",
+                          resample_day="Wednesday",
                           include_cot=True,
                           include_intraday=True,
-                          selected_cot_features=['positioning', 'extremes', 'market_structure', 'flows'],
-                          selected_indicators=['moving_averages', 'macd', 'momentum'])
+                          selected_cot_features=['positioning', 'market_structure', 'flows'],
+                          selected_indicators=['moving_averages', 'macd'], normalize_momentum=True)
 
 forecast.features.dropna(axis=0, inplace=True)
-res = forecast.train_model(model_type='ridge', target_type='positioning', forecast_horizon=7, alpha=0.7, l1_ratio=0.4)
-new = forecast.run_selected_features(res['model'], model_type='ridge', alpha=0.8, l1_ratio=0.2, top_n=20)
+lasso = forecast.train_model(model_type='lasso', target_type='positioning', forecast_horizon=7, alpha=0.05, l1_ratio=0.1)
+ridge_light = forecast.train_model(model_type='ridge', target_type='positioning', forecast_horizon=6, alpha = 0.7)
