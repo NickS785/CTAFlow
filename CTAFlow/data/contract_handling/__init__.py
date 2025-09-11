69ae9e74cc227840d0b9615809c466218594ac79
MrGrayTheET <sandersnicholas68@gmail.com>
2025-09-11 11:44:55 -0500

    Added more analysis features, fixed bottlenecks

---
+++ CTAFlow/data/contract_handling/__init__.py
@@ -0,0 +1,12 @@
+from .curve_manager import Contract, SpreadData, SpreadFeature, SeqData, SpreadReturns, FuturesCurveManager, ExpiryTracker
+
+__all__ = [
+    "Contract",
+    "SeqData",
+    "SpreadData",
+    "ExpiryTracker",
+    "SpreadReturns",
+    "SpreadFeature",
+    "FuturesCurveManager",
+
+]
