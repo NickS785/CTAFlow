ddecd2277fb07d97ec28c2a3da37a61c91894b61
MrGrayTheET <sandersnicholas68@gmail.com>
2025-09-18 10:39:29 -0500

    Added constant maturity to CurveEvolutionAnalysis

--- CTAFlow/main.py
+++ CTAFlow/main.py
@@ -1,7 +1,7 @@
 from talib import MA, SMA, EMA, MOM
 import pandas as pd
 import numpy as np
-from CTAFlow.utils import TenorInterpolator, PCAAnalyzer, create_tenor_grid, vol_weighted_returns, deseasonalize_monthly
+from CTAFlow.utils import create_tenor_grid, vol_weighted_returns, deseasonalize_monthly
 
 from CTAFlow.features import CurveEvolutionAnalyzer
 from CTAFlow.data import SpreadData
@@ -10,12 +10,7 @@ import plotly.io as pio
 
 pio.renderers.default = "browser"
 
-const_maturity = pd.read_csv("C:\\Users\\nicho\PycharmProjects\CTAFlow\\NG_constant_maturity_interpolated.csv",)
-const_maturity.values[:, 1:] = const_maturity.values[:, 1:].astype(float)
-const_maturity.set_index(pd.to_datetime(const_maturity['date']), inplace=True, drop=True)
-const_maturity.drop(columns='date', inplace=True)
-
-returns = np.log(const_maturity/const_maturity.shift(1))
-
-vol_weighted_ret = vol_weighted_returns(returns)
-deseasonalized_ret = deseasonalize_monthly(vol_weighted_ret.values, vol_weighted_ret.index)
+cl_f = SpreadData("NG")
+clanalyze = CurveEvolutionAnalyzer(cl_f)
+taus = create_tenor_grid(1/4, 3.0, 'quarterly')
+clanalyze.setup_constant_maturity(taus)
