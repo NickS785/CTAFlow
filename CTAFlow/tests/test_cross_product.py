7c30ea78eacf848df5ffedea28b1edd9e34846a7
MrGrayTheET <sandersnicholas68@gmail.com>
2025-10-12 16:39:18 -0500

    Updated Historical intraday momentum/seasonal screener

--- CTAFlow/tests/test_cross_product.py
+++
@@ -1,7 +0,0 @@
-from CTAFlow.data import DataClient, CrossProductEngine, CrossSpreadLeg
-
-rb_leg = CrossSpreadLeg.load_from_dclient("RB", base_weight=2.0, start_date="2020-01-1")
-ho_leg = CrossSpreadLeg.load_from_dclient("HO", base_weight=1.0, start_date="2020-01-01")
-cl_leg = CrossSpreadLeg.load_from_dclient("CL", base_weight=-3.0, start_date="2020-01-01")
-
-crack_spread = CrossProductEngine.from_legs([cl_leg, ho_leg, rb_leg])
