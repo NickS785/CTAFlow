9c07e435ee30b204c760cac6dce215d62054d81a
Nick S. <sandersnicholas68@gmail.com>
2025-12-27 13:55:18 -0600

    Add files via upload

---
+++ CTAFlow/data/model_datasets.py
@@ -0,0 +1,38 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import numpy as np
+
+
+class MomentumWindowDataset(Dataset):
+    def __init__(self, X_df: pd.DataFrame, y: pd.Series, lookback: int = 20):
+        X_df = X_df.sort_index()
+        y = y.sort_index()
+
+        # strict alignment
+        common = X_df.index.intersection(y.index)
+        X_df = X_df.loc[common]
+        y = y.loc[common]
+
+        # basic cleanup (choose your own policy)
+        X_df = X_df.replace([np.inf, -np.inf], np.nan).ffill().bfill()
+        self.X = X_df.to_numpy(dtype=np.float32)
+        self.y = y.to_numpy(dtype=np.float32)
+        self.lookback = lookback
+        self.n_features = self.X.shape[1]
+
+    def __len__(self):
+        return max(0, len(self.y) - self.lookback + 1)
+
+    def __getitem__(self, idx):
+        # window ends at idx + lookback - 1
+        sl = slice(idx, idx + self.lookback)
+        x_win = self.X[sl]                      # (L, C)
+        x_win = torch.from_numpy(x_win).T       # (C, L)
+        y_t = torch.tensor(self.y[idx + self.lookback - 1])
+        return x_win, y_t
+
+def make_window_dataset(X_df, y, lookback=20, batch_size=64):
+    ds = MomentumWindowDataset(X_df, y, lookback=lookback)
+    dl = DataLoader(ds, batch_size=batch_size, shuffle=False, drop_last=False)
+    return ds, dl
